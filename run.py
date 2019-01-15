0ff5bcb4d7d18232a987705cdd88ae19de7236b4
Benito <contact@benitopatino.com>
2019-01-15 13:31:24 -0800

    Create templates folder and Home Template

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1f55a181c9357b6ef3f0b18d331de757'    
+    url = 'http://api.openweathermap.org/data/2.5/weather?zip=95987,us&units=imperial&appid=1f55a181c9357b6ef3f0b18d331de757'    
     req = requests.get(url)
-
- 
-    name = req.json()['name']
-    weather = req.json()['weather'][0]['main']
+    jsonReq = req.json()
     
-    return weather
+    city = {}
+    city['name'] = jsonReq['name']
+    city['country'] = jsonReq['sys']['country']
+    city['temp'] = jsonReq['main']['temp']
 
-    
+    return render_template('index.html', title='Home', city=city)
